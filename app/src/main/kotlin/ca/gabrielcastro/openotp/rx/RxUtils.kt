5abe1d74e8a676f20548e6620c31bacb7f4094ff
Gabriel Castro <dev@GabrielCastro.ca>
2016-06-20 21:37:44 -0400

    add recycler view for list

--- app/src/main/kotlin/ca/gabrielcastro/openotp/rx/RxUtils.kt
+++ app/src/main/kotlin/ca/gabrielcastro/openotp/rx/RxUtils.kt
@@ -1,10 +1,11 @@
 package ca.gabrielcastro.openotp.rx
 
 import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 
 fun <T> Observable<T>.ioAndMain(): Observable<T> {
     return this.subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
 }
--- app/src/main/kotlin/ca/gabrielcastro/openotp/ui/list/OtpListActivity.kt
+++ app/src/main/kotlin/ca/gabrielcastro/openotp/ui/list/OtpListActivity.kt
@@ -1,10 +1,15 @@
 package ca.gabrielcastro.openotp.ui.list
 
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import ca.gabrielcastro.openotp.R
 import ca.gabrielcastro.openotp.app.App
 import ca.gabrielcastro.openotp.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.otp_list_activity.*
+import kotlinx.android.synthetic.main.otp_list_item.view.*
+import java.util.*
 import javax.inject.Inject
 
 class OtpListActivity : BaseActivity(), ListContract.View {
@@ -17,6 +22,8 @@ class OtpListActivity : BaseActivity(), ListContract.View {
         setContentView(R.layout.otp_list_activity)
         App.component(this).listComponent.inject(this)
         presenter.init(this)
+
+        otp_list_recycler.adapter = Adapter()
     }
 
     override fun onResume() {
@@ -30,9 +37,46 @@ class OtpListActivity : BaseActivity(), ListContract.View {
     }
 
     override fun showItems(items: List<ListContract.ListItem>) {
-        val str = items.map { it.toString() }
-                .joinToString("\n\n\t")
-        textView.text = str
+        val adapter = otp_list_recycler.adapter as Adapter
+        adapter.items = items
+        adapter.notifyDataSetChanged()
+    }
+
+}
+
+private class Holder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.otp_list_item, parent, false)
+) {
+
+    init {
+        itemView.otp_item_card.setOnClickListener {
+            //
+        }
+    }
+
+    var item: ListContract.ListItem? = null
+        set(value) {
+            field = value
+            itemView.otp_item_issuer.text = field?.issuer
+            itemView.otp_item_account.text = field?.account
+        }
+
+}
+
+private class Adapter : RecyclerView.Adapter<Holder>() {
+
+    var items: List<ListContract.ListItem> = Collections.emptyList()
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(parent)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.item = items[position]
     }
 
 }
