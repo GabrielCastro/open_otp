d497722bf3b28c408e72c642e8e14b5fa7db257c
Gabriel Castro <dev@GabrielCastro.ca>
2016-06-20 22:41:07 -0400

    remove LeakCanary from roboelectric

--- app/src/debug/kotlin/ca/gabrielcastro/openotp/app/DebugInitilizer.kt
+++ app/src/debug/kotlin/ca/gabrielcastro/openotp/app/DebugInitilizer.kt
@@ -1,12 +1,20 @@
 package ca.gabrielcastro.openotp.app
 
+import android.os.Build
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
 import dagger.Module
 import dagger.Provides
 
+private fun isRoboUnitTest() : Boolean {
+    return "robolectric".equals(Build.FINGERPRINT);
+}
+
 class DebugInitializer : Initializer {
     override fun init(app: App) {
+        if (isRoboUnitTest()) {
+            return
+        }
         LeakCanary.install(app)
         Stetho.initializeWithDefaults(app)
     }
