bd60d255b6f4b6ed3705d2ed7ac72bf853fbe6ff
Gabriel Castro <dev@GabrielCastro.ca>
2016-06-26 21:28:01 -0400

    add shows detail test to list presenter

--- app/src/test/kotlin/ca/gabrielcastro/openotp/ui/list/ListPresenterImplTest.kt
+++ app/src/test/kotlin/ca/gabrielcastro/openotp/ui/list/ListPresenterImplTest.kt
@@ -2,6 +2,7 @@ package ca.gabrielcastro.openotp.ui.list
 
 import ca.gabrielcastro.openotp.testutils.FakeDatabase
 import ca.gabrielcastro.openotp.testutils.RxAndroidRule
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.*
@@ -13,13 +14,22 @@ class ListPresenterImplTest {
     @JvmField
     val rxRule = RxAndroidRule()
 
+    private var _db: FakeDatabase? = null
+    private var _view: ListContract.View? = null
+    private var _presenter: ListContract.Presenter? = null
+    val db: FakeDatabase get() = _db!!
+    val view: ListContract.View get() = _view!!
+    val presenter: ListContract.Presenter get() = _presenter!!
+
+    @Before
+    fun setUp() {
+        _db = FakeDatabase()
+        _view = mock(ListContract.View::class.java)
+        _presenter = ListPresenterImpl(db)
+    }
+
     @Test
     fun showsList() {
-        val db = FakeDatabase()
-        val view = mock(ListContract.View::class.java)
-
-        val presenter = ListPresenterImpl(db)
-
         presenter.init(view)
         presenter.resume()
 
@@ -28,5 +38,19 @@ class ListPresenterImplTest {
         verify(view).showItems(anyListOf(ListContract.ListItem::class.java))
 
         presenter.pause()
+    }
+
+    @Test
+    fun showsDetail() {
+        presenter.init(view)
+
+        val item = ListContract.ListItem(
+                "123456789",
+                "issuer",
+                "account"
+        )
+        presenter.itemSelected(item)
+
+        verify(view).showDetailForId("123456789")
     }
 }
